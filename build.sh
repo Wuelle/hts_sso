4440178695dd84fe7c0bf0efce6191c892de6c43
Simon Wülker <simon.wuelker@arcor.de>
2022-10-23 22:23:13 +0200

    i forgot what i did but I'm sure it was important ^^

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 sass swagger_server/sass/main.scss swagger_server/static/_assets/css/bootstrap.css
+sass swagger_server/sass/dashboard.scss swagger_server/static/_assets/css/dashboard.css
 sudo docker run $(sudo docker build . -q)
--- swagger_server/static/_assets/js/dashboard.js
+++ swagger_server/static/_assets/js/dashboard.js
@@ -6,6 +6,7 @@ $(document).ready(() => {
 function get_page(page_name) {
     return $("[data-page='" + page_name + "']");
 }
+
 function show_page(page_name) {
     $("[page-store]").append(get_page(active_page));
     $("[page-container]").append(get_page(page_name));
@@ -14,4 +15,24 @@ function show_page(page_name) {
 
 function on_profile_picture_click() {
     $("#profile-picture-upload").click();
+}
+
+function edit(name) {
+    let element = $("#" + name);
+    element.prop("contenteditable", true);
+
+    // set focus to end of value
+    let length = element.text().length;
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.setStart(element[0].childNodes[0], length);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    element.focus();
+}
+
+function stop_editing(name) {
+    $("#" + name).prop("contenteditable", false);
 }
